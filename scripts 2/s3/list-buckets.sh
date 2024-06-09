89326dcdc17024506dfb82b4b798cd995a68a94c
bronifty <bronifty@gmail.com>
2024-06-08 21:07:36 -0400

    this

---
+++ scripts 2/s3/list-buckets.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+# Get the directory where the script is located
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+
+# Source the scripts using the script directory path
+source $SCRIPT_DIR/../variables.sh
+
+# Check if both a function name and API name were passed as arguments
+if [ -z "$1" ]; then
+    echo "No profile name provided. Using default profile: $DEFAULT_PROFILE_NAME"
+    PROFILE_NAME=$DEFAULT_PROFILE_NAME
+else
+    PROFILE_NAME=$1
+fi
+   
+echo "aws s3api list-buckets --profile $PROFILE_NAME"
+aws s3api list-buckets --profile $PROFILE_NAME
+
+
+
+
