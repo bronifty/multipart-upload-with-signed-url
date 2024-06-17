1aa45706f235ab884f8b863d8ea9f03c6a228887
bronifty <bronifty@gmail.com>
2024-06-17 17:51:38 -0400

    this

--- scripts/admin-cloud/bootstrap.sh
+++ scripts/admin-cloud/bootstrap.sh
@@ -1,13 +1,20 @@
 #!/bin/bash
-# cdk bootstrap aws://851725517932/us-east-1
 # Get the directory where the script is located
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 
 # Source the scripts using the script directory path
 source $SCRIPT_DIR/../variables.sh
 
-account_id=$(aws sts get-caller-identity --query "Account" --output text)
-region=$(aws configure get region)
-./scripts/cloud-admin/cdk.sh $account_id $region
+# Check if both a function name and API name were passed as arguments
 
+if [ -z "$1" ]; then
+    echo "No profile name provided. Using default profile: $DEFAULT_PROFILE_NAME"
+    PROFILE_NAME=$DEFAULT_PROFILE_NAME
+else
+    PROFILE_NAME=$1
+fi
 
+account_id=$(aws sts get-caller-identity --profile $PROFILE_NAME --query "Account" --output text)
+region=$(aws configure get region --profile $PROFILE_NAME)
+
+pnpx cdk bootstrap aws://$account_id/$region
