d066606e81146386a201c441422fa80cb596cc8f
bronifty <bronifty@gmail.com>
2024-06-17 17:52:30 -0400

    this

--- scripts/admin-cloud/login.sh
+++ scripts/admin-cloud/login.sh
@@ -4,5 +4,15 @@ SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 
 # Source the scripts using the script directory path
 source $SCRIPT_DIR/../variables.sh
+
+# Check if both a function name and API name were passed as arguments
+
+if [ -z "$1" ]; then
+    echo "No profile name provided. Using default profile: $DEFAULT_PROFILE_NAME"
+    PROFILE_NAME=$DEFAULT_PROFILE_NAME
+else
+    PROFILE_NAME=$1
+fi
+
 # Login to AWS SSO
-aws sso login 
+aws sso login --profile $PROFILE_NAME
