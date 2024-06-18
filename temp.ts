e78ffba7b9715ae6b7a6e041591c2d397f6d148f
bronifty <bronifty@gmail.com>
2024-06-18 18:49:45 -0400

    this

--- temp.ts
+++ temp.ts
@@ -5,11 +5,50 @@ import { dirname } from "node:path";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
-console.log(__dirname, __filename);
+// const dependencyAsset = import.meta.resolve("./cdk/README.md");
+// console.log(dependencyAsset);
 
-const dependencyAsset = import.meta.resolve("./cdk/README.md");
-console.log(dependencyAsset);
+// const cdkDir = new URL("./cdk/", import.meta.url);
 
-// file:///app/node_modules/component-lib/asset.css
-// import.meta.resolve("./dep.js");
-// file:///app/dep.js
+// function resolveCdk(path = "") {
+//   return fileURLToPath(new URL(path, cdkDir));
+// }
+// const cdkFilePath = resolveCdk("README.md");
+// console.log("cdkFilePath", cdkFilePath);
+
+// const reactComponentRegex = /\.jsx$/;
+
+// import.meta.url; // old way
+// import.meta.filename; // fileURLToPath(import.meta.url)
+// import.meta.dirname; // dirname(import.meta.filename)
+// import.meta.resolve(specifier);
+
+// old way to specify an absolute filepath relative to current directory
+console.log(
+  "old way to specify an absolute filepath relative to current directory: ",
+  fileURLToPath(new URL("./cdk/", import.meta.url))
+);
+// new way to specify an absolute filepath relative to current directory
+console.log(
+  "new way to specify an absolute filepath relative to current directory",
+  `${import.meta.dirname}/cdk`
+);
+// way to specify a filepath with the file protocol
+const fileProtocolSpecifier = import.meta.resolve("./cdk/README.md");
+console.log(
+  "new way to specify a filepath with the file protocol",
+  fileProtocolSpecifier
+);
+// old way to parameterize a filepath
+const cdkDir = new URL("./cdk/", import.meta.url);
+function resolveCdk(path = "") {
+  return fileURLToPath(new URL(path, cdkDir));
+}
+const cdkFilePath = resolveCdk("README.md");
+console.log("old way to parameterize a filepath", cdkFilePath);
+// new way to parameterize a filepath
+function resolveCdkMeta(path = "") {
+  return import.meta.resolve(path);
+}
+const cdkFilePathMeta = resolveCdkMeta("./cdk/README.md");
+console.log("new way to parameterize a filepath", cdkFilePathMeta);
