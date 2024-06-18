408f906c6b6d457d7dbd7deb3c9a7cf13be422f8
bronifty <bronifty@gmail.com>
2024-06-17 20:16:42 -0400

    this

--- aws-sdk/s3/listBuckets.ts
+++ aws-sdk/s3/listBuckets.ts
@@ -2,10 +2,14 @@ import { ListBucketsCommand } from "@aws-sdk/client-s3";
 import { createS3Client } from ".";
 
 // Function to delete a bucket
-async function listBuckets(profile: string): Promise<ListBucketsCommandOutput> {
+async function listBuckets(profile: string): Promise<{ Name: string }[]> {
   const s3Client = createS3Client(profile);
-  const buckets = await s3Client.send(new ListBucketsCommand());
-  return buckets;
+  const { Buckets } = await s3Client.send(new ListBucketsCommand());
+  return (
+    Buckets?.filter((bucket) => bucket.Name !== undefined).map((bucket) => ({
+      Name: bucket.Name as string, // Type assertion here
+    })) ?? []
+  );
 }
 
 export { listBuckets };
--- aws-sdk/s3/mainListBuckets.ts
+++ aws-sdk/s3/mainListBuckets.ts
@@ -3,9 +3,8 @@ import { listBuckets } from ".";
 async function mainListBuckets(profile: string = "default"): Promise<void> {
   const buckets = await listBuckets(profile);
   if (!buckets) return;
-  console.log(
-    `Buckets: ${buckets.Buckets.map((bucket) => bucket.Name).join(", ")}`
-  );
+  console.log(buckets);
+  console.log(`Buckets: ${buckets.map((bucket) => bucket.Name).join(", ")}`);
 }
 
 // Usage example
