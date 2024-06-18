9e1f11588246a488b1755906929f4ec4cef6ce29
bronifty <bronifty@gmail.com>
2024-06-17 21:03:06 -0400

    this

--- aws-sdk/s3/listObjects.ts
+++ aws-sdk/s3/listObjects.ts
@@ -4,12 +4,16 @@ import { createS3Client } from ".";
 async function listObjects(
   profile: string,
   bucketName: string
-): Promise<ListObjectsV2CommandOutput> {
+): Promise<{ Key: string }[]> {
   const s3Client = createS3Client(profile);
-  let objects = await s3Client.send(
+  const { Contents } = await s3Client.send(
     new ListObjectsV2Command({ Bucket: bucketName })
   );
-  return objects;
+  return (
+    Contents?.filter((file) => file.Key !== undefined).map((file) => ({
+      Key: file.Key as string, // Type assertion here
+    })) ?? []
+  );
 }
 
 export { listObjects };
