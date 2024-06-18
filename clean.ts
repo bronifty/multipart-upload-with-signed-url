af55c032ea105734d13aa8ec5cff93143e94bc98
bronifty <bronifty@gmail.com>
2024-06-18 15:45:04 -0400

    this

--- clean.ts
+++ clean.ts
@@ -1,28 +1,48 @@
-import { glob, unlink, rm } from "node:fs/promises";
-import fg from "fast-glob";
+import { glob, rm, lstat } from "node:fs/promises";
 
 (async () => {
   const directories = new Set();
-
-  // First, collect all directories that contain 'node_modules'
-  for await (const file of fg.stream(
-    `${import.meta.dirname}/**/node_modules/**/*`,
-    {
-      onlyDirectories: true,
-      followSymbolicLinks: true,
+  // Collect all paths that might contain node_modules
+  for await (const file of glob(`${import.meta.dirname}/**/*`)) {
+    if (file.includes("/node_modules/")) {
+      const stats = await lstat(file);
+      if (stats.isDirectory()) {
+        // This will capture the path up to and including 'node_modules'
+        const dirPath = file.split("/node_modules")[0] + "/node_modules";
+        directories.add(dirPath);
+      }
     }
-  )) {
-    // This will capture the path up to and including 'node_modules'
-    const dirPath = file.split("/node_modules")[0] + "/node_modules";
-    directories.add(dirPath);
   }
-
   // Now, remove each collected directory
   for (const dir of directories) {
     console.log(`Removing directory: ${dir}`);
     await rm(dir, { recursive: true, force: true });
   }
 })();
+// import fg from "fast-glob";
+
+// (async () => {
+//   const directories = new Set();
+
+//   // First, collect all directories that contain 'node_modules'
+//   for await (const file of fg.stream(
+//     `${import.meta.dirname}/**/node_modules/**/*`,
+//     {
+//       onlyDirectories: true,
+//       followSymbolicLinks: true,
+//     }
+//   )) {
+//     // This will capture the path up to and including 'node_modules'
+//     const dirPath = file.split("/node_modules")[0] + "/node_modules";
+//     directories.add(dirPath);
+//   }
+
+//   // Now, remove each collected directory
+//   for (const dir of directories) {
+//     console.log(`Removing directory: ${dir}`);
+//     await rm(dir, { recursive: true, force: true });
+//   }
+// })();
 
 // (async () => {
 //   const files = await fg(`${import.meta.dirname}/**/*`, {
@@ -30,10 +50,17 @@ import fg from "fast-glob";
 //   });
 //   files.forEach((file) => console.log(file));
 // })();
-
-// for await (const file of glob(`${import.meta.dirname}/**/*`)) {
-//   console.log(file);
-//     if (file.includes("/node_modules/")) {
-//       await rm(file, { recursive: true, force: true });
-//     }
-// }
+// (async () => {
+//   const directories = new Set();
+//   // collect all directories that contain node_modules
+//   for await (const file of glob(`${import.meta.dirname}/**/*`)) {
+//     // This will capture the path up to and including 'node_modules'
+//     const dirPath = file.split("/node_modules")[0] + "/node_modules";
+//     directories.add(dirPath);
+//   }
+//   // Now, remove each collected directory
+//   for (const dir of directories) {
+//     console.log(`Removing directory: ${dir}`);
+//     await rm(dir, { recursive: true, force: true });
+//   }
+// })();
