9b07a1b200ccee4f57248d916222d2413aa05d83
bronifty <bronifty@gmail.com>
2024-06-17 21:04:38 -0400

    this

--- aws-sdk/s3/mainListObjects.ts
+++ aws-sdk/s3/mainListObjects.ts
@@ -6,13 +6,3 @@ async function mainListObjects(profile: string, bucketName: string) {
 }
 
 mainListObjects("sst", "bronifty-sst").catch(console.error);
-
-// Contents: [
-//     {
-//       Key: 'Archive.zip',
-//       LastModified: 2024-06-18T00:50:34.000Z,
-//       ETag: '"116c1995614bb64368067a945244cdf8-3"',
-//       Size: 11551578,
-//       StorageClass: 'STANDARD'
-//     }
-//   ]
