c93f489c8b7a2005314b53eb85dc85a8ca86a444
bronifty <bronifty@gmail.com>
2024-06-18 22:43:59 -0400

    this

--- cdkv2/infra/dist/aws-sdk-js-notes-app.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const aws_cdk_lib_1 = require("aws-cdk-lib");
-const aws_sdk_js_notes_app_stack_1 = require("./aws-sdk-js-notes-app-stack");
-const app = new aws_cdk_lib_1.App();
-new aws_sdk_js_notes_app_stack_1.AwsSdkJsNotesAppStack(app, "aws-sdk-js-notes-app");
