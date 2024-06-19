f0cc18278ad975bb058472c677534bfd02e161dc
bronifty <bronifty@gmail.com>
2024-06-19 15:26:15 -0400

    this

--- cdkv3/infra/cdk/backend/libs/response.ts
+++ cdkv3/infra/cdk/backend/libs/response.ts
@@ -1,3 +1,16 @@
+const buildResponse = (statusCode: number, body: any) => ({
+  statusCode,
+  headers: {
+    "access-control-allow-origin": "*",
+    "access-control-allow-credentials": true,
+  },
+  body: JSON.stringify(body),
+});
+
 export const success = (body: any) => {
   return buildResponse(200, body);
+};
+
+export const failure = (body: any) => {
+  return buildResponse(500, body);
 };
