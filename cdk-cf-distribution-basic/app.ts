ce9b53a24ab5d19307d63458210c6743b27f0a31
bronifty <bronifty@gmail.com>
2024-07-01 22:40:42 -0400

    this

--- cdk-cf-distribution-basic/app.ts
+++ cdk-cf-distribution-basic/app.ts
@@ -3,11 +3,17 @@
 // import "source-map-support/register";
 import * as cdk from "aws-cdk-lib";
 
-import { AppStack } from "./stack";
+import { S3Stack } from "./s3-stack";
+import { LambdaStack } from "./lambda-stack";
 
 const app = new cdk.App();
 
-new AppStack(app, "AppStack", {
+new S3Stack(app, "S3Stack", {
   env: { region: "us-east-1" },
-  description: "App Stack",
+  description: "S3 Stack",
+});
+
+new LambdaStack(app, "LambdaStack", {
+  env: { region: "us-east-1" },
+  description: "Lambda Stack",
 });
--- cdk-cf-distribution-basic/s3-stack.ts
+++ cdk-cf-distribution-basic/s3-stack.ts
@@ -1,11 +1,17 @@
 import * as cdk from "aws-cdk-lib";
 import { Construct } from "constructs";
 
-export class AppStack extends cdk.Stack {
+export class S3Stack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
     const myBucket = new cdk.aws_s3.Bucket(this, "bronifty-site-bucket-2");
+
+    new cdk.aws_s3_deployment.BucketDeployment(this, "DeployWebsite", {
+      sources: [cdk.aws_s3_deployment.Source.asset("./app")],
+      destinationBucket: myBucket,
+    });
+
     new cdk.aws_cloudfront.Distribution(this, "bronifty-site-distribution-2", {
       defaultBehavior: {
         origin: new cdk.aws_cloudfront_origins.S3Origin(myBucket),
@@ -14,9 +20,9 @@ export class AppStack extends cdk.Stack {
   }
 }
 
-const app = new cdk.App();
+// const app = new cdk.App();
 
-new AppStack(app, "AppStack", {
-  env: { region: "us-east-1" },
-  description: "App Stack",
-});
+// new AppStack(app, "AppStack", {
+//   env: { region: "us-east-1" },
+//   description: "App Stack",
+// });
